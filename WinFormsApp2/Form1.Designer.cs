8358994dbb26fe97cd39ab5fba4bba95acf95012
scwanniarachchi <sewminchandila2000@gmail.com>
2021-10-07 01:16:22 +0530

    Create the user interface of Cal

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿
+namespace WinFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lbltot = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblavg = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblcls = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(199, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(427, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Student Performance Calculator";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(84, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Module 1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(84, 153);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 32);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Module 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(84, 200);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(123, 32);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Module 3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(84, 248);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 32);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Module 4";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(84, 292);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 32);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Module 5";
+            // 
+            // txt1
+            // 
+            this.txt1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txt1.Location = new System.Drawing.Point(251, 100);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(213, 39);
+            this.txt1.TabIndex = 6;
+            // 
+            // txt2
+            // 
+            this.txt2.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txt2.Location = new System.Drawing.Point(251, 150);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(213, 39);
+            this.txt2.TabIndex = 7;
+            // 
+            // txt3
+            // 
+            this.txt3.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txt3.Location = new System.Drawing.Point(251, 200);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(213, 39);
+            this.txt3.TabIndex = 8;
+            // 
+            // txt4
+            // 
+            this.txt4.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txt4.Location = new System.Drawing.Point(251, 248);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(213, 39);
+            this.txt4.TabIndex = 9;
+            // 
+            // txt5
+            // 
+            this.txt5.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txt5.Location = new System.Drawing.Point(251, 293);
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(213, 39);
+            this.txt5.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(84, 372);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(70, 32);
+            this.label7.TabIndex = 11;
+            this.label7.Text = "Total";
+            // 
+            // lbltot
+            // 
+            this.lbltot.AutoSize = true;
+            this.lbltot.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbltot.Location = new System.Drawing.Point(282, 372);
+            this.lbltot.Name = "lbltot";
+            this.lbltot.Size = new System.Drawing.Size(0, 32);
+            this.lbltot.TabIndex = 12;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(84, 434);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(107, 32);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Average";
+            // 
+            // lblavg
+            // 
+            this.lblavg.AutoSize = true;
+            this.lblavg.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblavg.Location = new System.Drawing.Point(282, 434);
+            this.lblavg.Name = "lblavg";
+            this.lblavg.Size = new System.Drawing.Size(0, 32);
+            this.lblavg.TabIndex = 14;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(84, 486);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(71, 32);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Class";
+            // 
+            // lblcls
+            // 
+            this.lblcls.AutoSize = true;
+            this.lblcls.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblcls.Location = new System.Drawing.Point(267, 486);
+            this.lblcls.Name = "lblcls";
+            this.lblcls.Size = new System.Drawing.Size(0, 32);
+            this.lblcls.TabIndex = 16;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(579, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(155, 52);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(579, 171);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(155, 52);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 533);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblcls);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblavg);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.lbltot);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lbltot;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblavg;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblcls;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
